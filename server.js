d4ffd85da138c274750de68fc6d278ee2eaee172
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-08 17:20:44 -0500

    Add dotenv and refactor port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.use(cors());
 app.use(express.static('public'));
