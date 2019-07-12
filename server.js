9b51e2ec8a11bfc8e405b9f22bb9b750769052a3
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-11 23:56:23 -0500

    Add react scripts to remote storage for faster load

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use('/', express.static('public'));
+app.use('/scripts', express.static('scripts'));
 app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on ${port}`));
