d7b1fbb519e639f6929bf13f17c261c3d82938c9
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-12 11:50:11 -0500

    Add compression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
+app.use(compression());
 app.use('/', express.static('public'));
 app.use('/scripts', express.static('scripts'));
 app.use(express.json());
